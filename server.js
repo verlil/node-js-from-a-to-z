29c0f8d0cdc138e90a0513b6564b5b9508e7c5e1
Vernigora Lilia <vernigoralilia@gmail.com>
2019-04-29 13:40:34 +0300

    15 - Echo Server on Node.JS

--- server.js
+++ server.js
@@ -1,18 +1,18 @@
 var http = require('http');
-var util = require('util');
+var url = require('url');
 
-var server = new http.Server(); // EventEmitter
+var server = new http.Server(function (req, res) {
+    console.log(req.method, req.url);
+
+    var urlParsed = url.parse(req.url, true);
+    console.log(urlParsed);
+
+    if (urlParsed.pathname == '/echo' && urlParsed.query.message) {
+        res.end(urlParses.query.message);
+    } else {
+        res.statusCode = 404;
+        res.end('Pge not found');
+    }
+});
 
 server.listen(1337, '127.0.0.1');
-
-var counter = 0;
-
-var emit = server.emit;
-server.emit = function(event) {
-    console.log(event);
-    emit.apply(server, arguments);
-}
-
-server.on('request', function (req, res) {
-    res.end('Hello world! ' + ++counter);
-});
